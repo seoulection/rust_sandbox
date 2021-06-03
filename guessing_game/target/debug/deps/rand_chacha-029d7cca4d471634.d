ca93a7250347e718713502e4ebde4794de53232f
seoulection <6445410+seoulection@users.noreply.github.com>
2021-06-03 15:27:00 -0700

    adds rand crate

---
+++ guessing_game/target/debug/deps/rand_chacha-029d7cca4d471634.d
@@ -0,0 +1,9 @@
+/Users/cloud/coding/sandbox/rust_sandbox/guessing_game/target/debug/deps/rand_chacha-029d7cca4d471634.rmeta: /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/lib.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/chacha.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/guts.rs
+
+/Users/cloud/coding/sandbox/rust_sandbox/guessing_game/target/debug/deps/librand_chacha-029d7cca4d471634.rlib: /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/lib.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/chacha.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/guts.rs
+
+/Users/cloud/coding/sandbox/rust_sandbox/guessing_game/target/debug/deps/rand_chacha-029d7cca4d471634.d: /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/lib.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/chacha.rs /Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/guts.rs
+
+/Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/lib.rs:
+/Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/chacha.rs:
+/Users/cloud/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.0/src/guts.rs:
